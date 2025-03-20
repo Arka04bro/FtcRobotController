a55c218b60b62a92ae9b41c2db8b7ef5fd6cb598
berki-devs <berki.devs@gmail.com>
2025-03-20 11:45:31 +0500

    feat: add pidf test to slider

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tester/PIDF_Slider.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.opmode.tester;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+@Config
+@TeleOp
+public class PIDF_Slider extends OpMode {
+    private PIDController controller;
+
+    public static double p = 0, i = 0, d = 0;
+    private static double f = 0;
+
+    public static int target = 0;
+
+    private static final double ticks_in_degree = 20272.08 / 120.0;
+
+    private DcMotorEx slider_motor;
+
+    @Override
+    public void init() {
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        slider_motor = hardwareMap.get(DcMotorEx.class, "slider_motor0");
+    }
+
+    @Override
+    public void loop() {
+        controller.setPID(p, i, d);
+        int sliderPos = slider_motor.getCurrentPosition();
+        double pid = controller.calculate(sliderPos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+        double power = pid + ff;
+
+        slider_motor.setPower(power);
+
+        telemetry.addData("pos ", sliderPos);
+        telemetry.addData("target pos ", target);
+        telemetry.update();
+    }
+}
