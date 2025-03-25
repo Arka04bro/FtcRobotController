a0769c0b4b03ac0a666f26a84ed9f2511d670837
Arkat Khassanov <133187845+Arka04bro@users.noreply.github.com>
2025-03-25 22:41:37 +0500

    feat: add opencv class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tester/CAMERA/SampleDetectionTesting.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.opmode.tester.CAMERA;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot.Systems.Camera;
+
+@TeleOp
+public class SampleDetectionTesting extends LinearOpMode {
+
+
+    private Camera sampleCamera;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        sampleCamera = new Camera(hardwareMap, telemetry);
+
+        sampleCamera.findYellowSample();
+
+        while(opModeInInit()){
+            telemetry.addLine("Sample: " + sampleCamera.getTempX() + ", " + sampleCamera.getTempY() + ", "+ sampleCamera.getTempYaw());
+            telemetry.update();
+        }
+
+        while(opModeIsActive()){
+            telemetry.addLine("Sample: " + sampleCamera.getTempX() + ", " + sampleCamera.getTempY() + ", "+ sampleCamera.getTempYaw());
+            telemetry.update();
+        }
+    }
+
+}
