37ff4f50fe28f1ff4c0aa1e6bb123a47ad6d1d0e
berki-devs <berki.devs@gmail.com>
2025-04-02 22:08:18 +0500

    fix: rename configuration class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tester/PIDF_Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tester/PIDF_Config.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Config
 @TeleOp
-public class PIDF_Slider extends OpMode {
+public class PIDF_Config extends OpMode {
     private PIDController controller;
 
     public static double p = 0, i = 0, d = 0;
@@ -19,29 +19,29 @@ public class PIDF_Slider extends OpMode {
 
     public static int target = 0;
 
+    // For 5203 Series Yellow Jacket Planetary Gear Motor 223 RPM
     private static final double ticks_in_degree = 751.8/360.0; // 2,0883333333
 
-    private DcMotorEx slider_motor;
+    private DcMotorEx motorEx;
 
     @Override
     public void init() {
-
         controller = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        slider_motor = hardwareMap.get(DcMotorEx.class, "slider_motor0");
+        motorEx = hardwareMap.get(DcMotorEx.class, "Motor_0");
     }
 
     @Override
     public void loop() {
         controller.setPID(p, i, d);
-        int sliderPos = slider_motor.getCurrentPosition();
+        int sliderPos = motorEx.getCurrentPosition();
         double pid = controller.calculate(sliderPos, target);
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
         double power = pid + ff;
 
-        slider_motor.setPower(power);
+        motorEx.setPower(power);
 
         telemetry.addData("pos ", sliderPos);
         telemetry.addData("target pos ", target);
