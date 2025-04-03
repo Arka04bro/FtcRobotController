61bb309cddbeab137ade3d97ee937e64538b8f52
berki-devs <berki.devs@gmail.com>
2025-04-03 19:12:19 +0500

    fix: reduce memo leak

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/vision/SampleDetectionPipelineConfigure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/vision/SampleDetectionPipelineConfigure.java
@@ -56,19 +56,28 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
 
     public static int MIN_CONTOUR_LENGTH = 290;
     public static int MAX_CONTOUR_LENGTH = 675;
-
     public static double MIN_DISTANCE = 65;
     public static double PIX2INCHES = 0.015;
 
+    // NOTE: Optimize garbage collector and reduce memory leak
+    private final Mat hsvFrame = new Mat();
+    private final Mat boundMask = new Mat();
+    private final Mat hierarchy = new Mat();
+    private final Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+    private final Mat mask1 = new Mat(), mask2 = new Mat();
+
+    private final List<MatOfPoint> contours = new ArrayList<>();
     private final List<Point> foundSamplePositionsPix = new ArrayList<>();
     private final List<Point> foundSamplePositionsInches = new ArrayList<>();
     private final List<Double> foundSampleRotations = new ArrayList<>();
+    private final MatOfPoint2f contour2f = new MatOfPoint2f();
+    private final MatOfPoint2f approxContour2f = new MatOfPoint2f();
 
     private static class Sample {
         private final Point centerInInches;
         private final double rotation;
 
-        public Sample( Point centerInInches, double rotation) {
+        public Sample(Point centerInInches, double rotation) {
             this.centerInInches = centerInInches;
             this.rotation = rotation;
         }
@@ -85,11 +94,7 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
     @Override
     public Mat processFrame(Mat input) {
         Mat mask = preprocessFrame(input);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
         Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
         Sample bestSample = findLargestContour(contours, input);
 
         for (MatOfPoint contour : contours) {
@@ -104,43 +109,26 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
             Imgproc.circle(input, bestSample.getCenterInInches(), 25, new Scalar(0, 255, 0), 9);
         }
 
+        releaseResources();
+
         return showMask ? mask : input;
     }
 
-    // TODO: Optimize garbage collector
     private Mat preprocessFrame(Mat frame) {
-        Mat hsvFrame = new Mat();
         Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_BGR2HSV);
-
-        Mat boundMask = new Mat();
-
-        // 0 - red, 1 - yellow, 2 - blue
         switch (trackingColorChoice) {
-            case 1: // yellow
-                Scalar yellowLower = new Scalar(YellowHL, YellowSL, YellowVL);
-                Scalar yellowUpper = new Scalar(YellowHH, YellowSH, YellowVH);
-                Core.inRange(hsvFrame, yellowLower, yellowUpper, boundMask);
+            case 1:
+                Core.inRange(hsvFrame, new Scalar(YellowHL, YellowSL, YellowVL), new Scalar(YellowHH, YellowSH, YellowVH), boundMask);
                 break;
-            case 2: // blue
-                Scalar blueLower = new Scalar(BlueHL, BlueSL, BlueVL);
-                Scalar blueUpper = new Scalar(BlueHH, BlueSH, BlueVH);
-                Core.inRange(hsvFrame, blueLower, blueUpper, boundMask);
+            case 2:
+                Core.inRange(hsvFrame, new Scalar(BlueHL, BlueSL, BlueVL), new Scalar(BlueHH, BlueSH, BlueVH), boundMask);
                 break;
-            case 0: // red is default
             default:
-                Scalar redLower1 = new Scalar(Red1HL, Red1SL, Red1VL);
-                Scalar redUpper1 = new Scalar(Red1HH, Red1SH, Red1VH);
-                Scalar redLower2 = new Scalar(Red2HL, Red2SL, Red2VL);
-                Scalar redUpper2 = new Scalar(Red2HH, Red2SH, Red2VH);
-                Mat mask1 = new Mat();
-                Mat mask2 = new Mat();
-                Core.inRange(hsvFrame, redLower1, redUpper1, mask1);
-                Core.inRange(hsvFrame, redLower2, redUpper2, mask2);
+                Core.inRange(hsvFrame, new Scalar(Red1HL, Red1SL, Red1VL), new Scalar(Red1HH, Red1SH, Red1VH), mask1);
+                Core.inRange(hsvFrame, new Scalar(Red2HL, Red2SL, Red2VL), new Scalar(Red2HH, Red2SH, Red2VH), mask2);
                 Core.bitwise_or(mask1, mask2, boundMask);
                 break;
         }
-
-        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.morphologyEx(boundMask, boundMask, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(boundMask, boundMask, Imgproc.MORPH_CLOSE, kernel);
 
@@ -163,47 +151,33 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
         foundSampleRotations.clear();
 
         for (MatOfPoint contour : contours) {
-            MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+            contour2f.fromArray(contour.toArray());
             double arcLen = Imgproc.arcLength(contour2f, true);
-            if (arcLen < MIN_CONTOUR_LENGTH || arcLen > MAX_CONTOUR_LENGTH) {
-                continue;
-            }
+            if (arcLen < MIN_CONTOUR_LENGTH || arcLen > MAX_CONTOUR_LENGTH) continue;
 
-            MatOfPoint2f approxContour2f = new MatOfPoint2f();
             Imgproc.approxPolyDP(contour2f, approxContour2f, arcLen / 22.0, true);
             List<Point> approxPoints = approxContour2f.toList();
-            if (approxPoints.isEmpty()) {
-                continue;
-            }
+            if (approxPoints.isEmpty()) continue;
+
             MatOfPoint approxContour = new MatOfPoint();
             approxContour.fromList(approxPoints);
 
             int sumX = 0, sumY = 0;
-            List<Point> pts = approxContour.toList();
-            for (Point pt : pts) {
+            for (Point pt : approxContour.toList()) {
                 sumX += (int) pt.x;
                 sumY += (int) pt.y;
             }
-            int avgX = sumX / pts.size();
-            int avgY = sumY / pts.size();
-            Point centerPix = new Point(avgX, avgY);
+            Point centerPix = new Point((double) sumX / approxPoints.size(), (double) sumY / approxPoints.size());
 
-            boolean originalPos = true;
-            for (Point s : foundSamplePositionsPix) {
-                if (distance(centerPix, s) < MIN_DISTANCE) {
-                    originalPos = false;
-                    break;
-                }
-            }
-            if (!originalPos)
-                continue;
+            boolean unique = foundSamplePositionsPix.stream().noneMatch(p -> distance(centerPix, p) < MIN_DISTANCE);
+            if (!unique) continue;
             foundSamplePositionsPix.add(centerPix);
 
-            double bestDistance = 0.0;
+            double bestDistance = 0;
             Point pos1 = new Point(), pos2 = new Point();
-            for (int i = 0; i < pts.size(); i++) {
-                Point p1 = pts.get(i);
-                Point p2 = pts.get((i + 1) % pts.size());
+            for (int i = 0; i < approxPoints.size(); i++) {
+                Point p1 = approxPoints.get(i);
+                Point p2 = approxPoints.get((i + 1) % approxPoints.size());
                 double d = distance(p1, p2);
                 if (d > bestDistance) {
                     bestDistance = d;
@@ -211,27 +185,18 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
                     pos2 = p2;
                 }
             }
-            Point angleP1 = (pos1.x < pos2.x) ? pos1 : pos2;
-            Point angleP2 = (pos1.x < pos2.x) ? pos2 : pos1;
-            double difX = angleP2.x - angleP1.x;
-            double difY = angleP2.y - angleP1.y;
-            double sampleAngle = Math.atan2(difX, difY) * (180 / Math.PI) - 180;
+            double sampleAngle = Math.atan2(pos2.x - pos1.x, pos2.y - pos1.y) * (180 / Math.PI) - 180;
             foundSampleRotations.add(sampleAngle);
 
-            double imageCenterX = baseImage.size().width / 2.0;
-            double imageCenterY = baseImage.size().height / 2.0;
-            double inchPosX = (centerPix.x - imageCenterX) * PIX2INCHES;
-            double inchPosY = (centerPix.y - imageCenterY) * -PIX2INCHES;
-            Point centerInInches = new Point(inchPosX, inchPosY);
+            double imageCenterX = baseImage.cols() / 2.0;
+            double imageCenterY = baseImage.rows() / 2.0;
+            Point centerInInches = new Point((centerPix.x - imageCenterX) * PIX2INCHES, (centerPix.y - imageCenterY) * -PIX2INCHES);
             foundSamplePositionsInches.add(centerInInches);
         }
 
-        if (foundSamplePositionsInches.isEmpty()) {
-            return null;
-        }
-
-        double closestDist = Double.MAX_VALUE;
+        if (foundSamplePositionsInches.isEmpty()) return null;
         int bestIndex = 0;
+        double closestDist = Double.MAX_VALUE;
         for (int i = 0; i < foundSamplePositionsInches.size(); i++) {
             double d = distance(foundSamplePositionsInches.get(i), new Point(0, 0));
             if (d < closestDist) {
@@ -239,7 +204,6 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
                 bestIndex = i;
             }
         }
-
         return new Sample(foundSamplePositionsInches.get(bestIndex), foundSampleRotations.get(bestIndex));
     }
 
@@ -247,4 +211,18 @@ public class SampleDetectionPipelineConfigure extends OpenCvPipeline {
         return Math.hypot(two.x - one.x, two.y - one.y);
     }
 
+    public void releaseResources() {
+        hsvFrame.release();
+        boundMask.release();
+        kernel.release();
+        hierarchy.release();
+        mask1.release();
+        mask2.release();
+        contours.clear();
+        foundSamplePositionsPix.clear();
+        foundSamplePositionsInches.clear();
+        foundSampleRotations.clear();
+        contour2f.release();
+        approxContour2f.release();
+    }
 }
